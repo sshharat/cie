37401da26c0c56b98fd3af03e175df95c06a96a6
veeres-javali18 <sharatjoshi1@gmail.com>
2025-04-09 17:04:53 +0530

    first commit

--- voting.java
+++ voting.java
@@ -7,10 +7,11 @@ public class voting {
         System.out.println("Enter your age:");
         int age = scanner.nextInt();
         
-        if (age >= 18) {
+        if (age > 18) {
             System.out.println("Eligible for voting");
-        } else {
+        }else {
             System.out.println("Not eligible for voting");
+            System.out.println("master");
         }
         
         scanner.close();
